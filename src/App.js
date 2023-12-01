7a66eecb30c8f333117b4b43841ed3f75e612ebc
Serhii-Kuzmin <seryy8248@gmail.com>
2023-12-01 18:39:02 +0100

    task 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { BlogCard } from "./components/BlogCard";
 import { Header } from "./components/Header";
+import dataArticle from "./assets/article.json";
 
 function App() {
   return (
     <div>
       <Header title="Project-js" />
+      <BlogCard {...dataArticle} />
     </div>
   );
 }
